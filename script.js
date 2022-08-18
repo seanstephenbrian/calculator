ac4bada2781f4fa95a0d427839fc139e35e2c7da
sean stephen brian <seanstephenbrian@gmail.com>
2022-08-18 13:09:24 -0400

    Solve the click '=' w/o storedValue problem
    Solve the problem outlined in the previous commit: now user can click
    '=' after clicking an operator and run the operation with the display
    value as both x and y in the operate() function; still encountering
    an issue with this situation and the 'subtract' operator, though: the
    calculator doesn't go down into the negatives if the user keeps clicking
    '=', instead it just toggles between 0 and the initial display value.
    
    Delete earlier code that is no longer in use.

--- script.js
+++ script.js
@@ -26,16 +26,14 @@ headerImg.addEventListener('mouseout', () => {
 const displayField = document.querySelector('.display-field');
 displayField.textContent = 0;
 
-let chosenOperator;
+let clickedNumber;
+let clickedOperator;
+let displayValue = displayField.textContent;
+let firstClick = true;
+let lastClicked = '';
 let storedValue;
 let storedOperator;
-let displayValue = displayField.textContent;
-let clickedOperator;
-let clickedNumber;
-let firstClick = true;
-let clickedEquals = false;
-let lastClicked = '';
-let error = false;
+let tempValue;
 
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
@@ -156,7 +154,7 @@ function updateDisplay() {
             if (clickedOperator === '=' && storedOperator && storedValue) {
                 console.log('four triggered');
     
-                tempValue = displayField.textContent;
+                tempValue = parseFloat(displayField.textContent);
                 console.log({storedOperator});
                 console.log({storedValue});
                 console.log({displayValue});
@@ -165,10 +163,11 @@ function updateDisplay() {
             }
 
             if (clickedOperator === '=' && storedOperator && !storedValue) {
-                console.log('five triggered');
+                console.log('TARGET triggered');
+                console.log(tempValue);
     
                 if (!tempValue) {
-                    tempValue = displayField.textContent;
+                    tempValue = parseFloat(displayField.textContent);
                 }
                 displayField.textContent = operate(storedOperator, tempValue, displayValue);
             }
@@ -179,158 +178,11 @@ function updateDisplay() {
     
         }
 
-
-
-        
-
-        
         lastClicked = clickedOperator;
         clickedOperator = '';
 
-    }
-        
+    }        
 }
-
-
-        // new attempt:
-
-        // if (firstClick) {
-        //     displayField.textContent = clickedNumber;
-        //     firstClick = false;
-        // } else if (displayField.textContent === '+' || displayField.textContent === '-' || displayField.textContent === 'x' || displayField.textContent === '÷' ) {
-        //     displayField.textContent = '';   
-        //     displayField.textContent += clickedNumber;
-        // } else {
-        //     displayField.textContent += clickedNumber;
-        // }
-
-
-        // first version:
-
-        // if (previousValue === '0' && !(storedValue)) {
-        //     firstClick = true;
-        //     displayField.textContent = clickedNumber;
-        // }
-        // else if ( ( (storedValue) && (parseFloat(displayField.textContent) === storedValue) ) || ( (!storedValue) && (displayField.textContent) && (firstClick) ) ) {
-        //     firstClick = false;
-        //     displayField.textContent = '';
-        //     displayField.textContent = clickedNumber;
-        // }
-        // else if (clickedOperator === '=') {
-        //     clickedEquals = true;
-        //     clickedOperator = '';
-        //     displayField.textContent = '';
-        //     displayField.textContent = clickedNumber;
-        // } else {
-        //     displayField.textContent += clickedNumber;
-        // }
-    //});
-//});
-
-
-
-
-        //new attempt:
-
-        // if (!lastClick && clickedOperator !== '=') {
-        //     if (clickedOperator === '+') {
-        //         lastClick = '+';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = add;
-        //     } else if (clickedOperator === '-') {
-        //         lastClick = '-';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = subtract;
-        //     } else if (clickedOperator === 'x') {
-        //         lastClick = 'x';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = multiply;
-        //     } else if (clickedOperator === '÷') {
-        //         lastClick = '÷';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = divide;
-        //     }
-        // } else if (!lastClick && clickedOperator === '=') {
-        //     lastClick = '=';
-        //     storedValue = parseFloat(displayField.textContent);
-        // } else if ( (lastClick === '+' || lastClick === '-' || lastClick === 'x' || lastClick === '÷') && 
-        //             (clickedOperator === '=' || clickedOperator === '+' || clickedOperator === '-' || clickedOperator === 'x' || clickedOperator === '÷') ) {
-        //     displayField.textContent = 'ERROR';
-        //     error = true;
-        // } else {
-        //     if (clickedOperator === '+') {
-        //         lastClick = '+';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = '+';
-        //         chosenOperator = add;
-        //     } else if (clickedOperator === '-') {
-        //         lastClick = '-';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = '-';
-        //         chosenOperator = subtract;
-        //     } else if (clickedOperator === 'x') {
-        //         lastClick = 'x';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = 'x';
-        //         chosenOperator = multiply;
-        //     } else if (clickedOperator === '÷') {
-        //         lastClick = '÷';
-        //         storedValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = '÷';
-        //         chosenOperator = divide;
-        //     }
-        // }
-
-        // first version:
-        // if (!storedValue) {
-        //     if (clickedOperator === '+') {
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = add;
-        //     } else if (clickedOperator === '-') {
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = subtract;
-        //     } else if (clickedOperator === 'x') {
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = multiply;
-        //     } else if (clickedOperator === '÷') {
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = divide;
-        //     }
-        // } else if (storedValue) {
-        //     if (clickedOperator === '+') {
-        //         displayValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = operate(chosenOperator,displayValue,storedValue);
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = add;
-        //     } else if (clickedOperator === '-') {
-        //         displayValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = operate(chosenOperator,displayValue,storedValue);
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = subtract;
-        //     } else if (clickedOperator === 'x') {
-        //         displayValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = operate(chosenOperator,displayValue,storedValue);
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = multiply;
-        //     } else if (clickedOperator === '÷') {
-        //         displayValue = parseFloat(displayField.textContent);
-        //         displayField.textContent = operate(chosenOperator,displayValue,storedValue);
-        //         storedValue = parseFloat(displayField.textContent);
-        //         chosenOperator = divide;
-        //     }
-        // } if (clickedOperator === '=') {
-        //     if (clickedEquals === true) {
-        //         chosenOperator = '';
-        //     } else {
-        //     clickedEquals = true;
-        //     displayValue = parseFloat(displayField.textContent);
-        //     displayField.textContent = operate(chosenOperator,displayValue,storedValue);
-        //     storedValue = '';
-        //     }
-        // }
-
-    
-
 
 function add(x, y) {
     return x + y;
